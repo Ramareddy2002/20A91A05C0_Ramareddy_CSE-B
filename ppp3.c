4aa9924f9b9ad8295594c4cac70f2e2e1fc98ad8
Ramareddy2002 <84448206+Ramareddy2002@users.noreply.github.com>
2021-05-20 12:29:17 +0530

    Add files via upload

---
+++ ppp3.c
@@ -0,0 +1,10 @@
+#indentation error
+
+a=int(input('enter value')) 
+print(a) 
+if a>0:
+       print(number is positive) 
+       print(u entered positive number) 
+else   
+       print(number is negative)
+       print(u entered negative number) 
